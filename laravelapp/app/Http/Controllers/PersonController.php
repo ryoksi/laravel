ae76997af6916d05aac1f553e1436d15d4bb9c22
ryoksi <51893609+ryoksi@users.noreply.github.com>
2020-01-03 10:05:44 +0900

    更新処理をついか

--- laravelapp/app/Http/Controllers/PersonController.php
+++ laravelapp/app/Http/Controllers/PersonController.php
@@ -40,7 +40,22 @@ class PersonController extends Controller
       $person = new Person;
       $form=$request->all();
       unset($form['_token']);
-      $person->timestamps=false;//taimstampを無効か
+      $person->timestamps=false;//taimstampを無効化
+      $person->fill($form)->save();
+      return redirect('/person');
+    }
+
+    public function edit(Request $request) {
+      $person=Person::find($request->id);
+      return view('person.edit',['form'=>$person]);
+    }
+
+    public function update(Request $request) {
+      $this->validate($request,Person::$rules);
+      $person=Person::find($request->id);
+      $form=$request->all();
+      unset($form['_token']);
+      $person->timestamps=false;//taimstampを無効化
       $person->fill($form)->save();
       return redirect('/person');
     }
--- laravelapp/resources/views/person/edit.blade.php
+++ laravelapp/resources/views/person/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title','Person')
 @section('menubar')
   @parent
-  model,Personを使用した追加処理
+  model,Personを使用した更新処理
 @endsection
 
 @section('content')
@@ -17,19 +17,20 @@
     </div>
   @endif
   <table>
-    <form class="" action="/person/add" method="post">
+    <form class="" action="/person/edit" method="post">
       {{csrf_field()}}
+      <input type="hidden" name="id" value="{{$form->id}}">
       <tr>
         <th>name:</th>
-        <td><input type="text" name="name" value="{{old('name')}}"></td>
+        <td><input type="text" name="name" value="{{$form->name}}"></td>
       </tr>
       <tr>
         <th>mail:</th>
-        <td><input type="text" name="mail" value="{{old('mail')}}"></td>
+        <td><input type="text" name="mail" value="{{$form->mail}}"></td>
       </tr>
       <tr>
         <th>age:</th>
-        <td><input type="number" name="age" value="{{old('age')}}"></td>
+        <td><input type="number" name="age" value="{{$form->age}}"></td>
       </tr>
       <tr>
         <th></th>
